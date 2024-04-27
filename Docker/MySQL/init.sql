5b991c069fe2672d3be747c98821b6295943bfca
tomo-tarou <shadowmoon0721@icloud.com>
2024-04-27 23:47:32 +0900

    channelsテーブルとmessagesテーブルを追加

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -15,7 +15,32 @@ CREATE TABLE users (
     self_introduction text NOT NULL,
     is_admin boolean NOT NULL DEFAULT 0,
     created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 );
 
+CREATE TABLE channels (
+    id integer NOT NULL AUTO_INCREMENT,
+    description text NOT NULL,
+    name varchar(255) NOT NULL,
+    start_date date NOT NULL,
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE messages (
+    id integer NOT NULL AUTO_INCREMENT,
+    user_id integer,
+    channel_id integer,
+    message text,
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (channel_id) REFERENCES channels(id)
+);
+
 INSERT INTO users (name, email, crypted_password, self_introduction) VALUES ('chiikawa', 'chiikawa@example.com', '1111', 'フ！');
+INSERT INTO channels (description, name, start_date) VALUES ('superぼっち部屋へようこそ', 'superぼっち部屋', '2024-04-27');
+INSERT INTO messages (user_id, channel_id, message) VALUES (1, 1, 'ヤヤーンパパヒュパヒュパ');
