e0316c6cde6e5cf7a9c562921baf701f5c5fcff4
tomo-tarou <shadowmoon0721@icloud.com>
2024-04-29 02:01:39 +0900

    ユーザー登録機能実装（パスワード暗号化ok）

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,6 +1,15 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash, abort
+from flask_bcrypt import Bcrypt
+import secrets
+from datetime import timedelta
+import re
+
+from models import dbConnect
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+random_secret_key = secrets.token_hex(16)
+app.config['SECRET_KEY'] = random_secret_key
 
 # サインアップページの表示
 @app.route('/signup')
@@ -8,8 +17,33 @@ def signup():
     return render_template('registration/signup.html')
 
 # サインアップ処理
-# @app.route('/signup', methods=['POST'])
-# def userSignup():
+@app.route('/signup', methods=['POST'])
+def userSignup():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+
+    pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+    if name == '' or email == '' or password1 == '' or password2 == '':
+        flash('入力されていないフォームがあります')
+    elif password1 != password2:
+        flash('２つのパスワードの値が違います。')
+    elif re.match(pattern, email) is None:
+        flash('正しいメールアドレスの形式で入力してください')
+    else:
+        crypted_password = bcrypt.generate_password_hash(password1).decode('utf8')
+        DBuser = dbConnect.getUser(email)
+
+        if DBuser is not None:
+            flash('既に登録されています')
+        else:
+            UserId = dbConnect.createUser(name, email, crypted_password)
+            session['id'] = UserId
+            return redirect('/')
+    return redirect('/signup')
+
 
 # ログインページの表示
 @app.route('/login')
