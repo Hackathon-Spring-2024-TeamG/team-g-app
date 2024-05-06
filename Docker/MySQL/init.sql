aacb545180b2c2e4ee9089c90274fb5ac1afeae3
tomo-tarou <shadowmoon0721@icloud.com>
2024-05-07 00:24:18 +0900

    badgesテーブルからバッジタイプ管理のために、badge_typesテーブルを切り出し

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -16,7 +16,7 @@ CREATE TABLE users (
     is_admin boolean NOT NULL DEFAULT 0,
     created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE channels (
@@ -25,7 +25,7 @@ CREATE TABLE channels (
     name varchar(255) NOT NULL,
     start_date date NOT NULL,
     created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE messages (
@@ -34,18 +34,29 @@ CREATE TABLE messages (
     channel_id integer NOT NULL,
     message text NOT NULL,
     created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(id),
+    PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (channel_id) REFERENCES channels(id) ON DELETE CASCADE
 );
 
+CREATE TABLE badge_types (
+    id integer NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE badges (
+    id integer NOT NULL AUTO_INCREMENT,
     user_id integer NOT NULL,
     message_id integer NOT NULL,
-    badge_type enum('GOLD', 'SILVER', 'BRONZE'),
+    badge_type_id integer NOT NULL,
+    created_by integer NULL,
     created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (message_id) REFERENCES messages(id) ON DELETE CASCADE
+    FOREIGN KEY (message_id) REFERENCES messages(id) ON DELETE CASCADE,
+    FOREIGN KEY (badge_type_id) REFERENCES badge_types(id),
+    FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE SET NULL
 );
 
 CREATE TABLE personal_channels (
@@ -54,7 +65,7 @@ CREATE TABLE personal_channels (
     name varchar(255) NOT NULL,
     description text,
     created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(id),
+    PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
@@ -62,5 +73,6 @@ INSERT INTO users (name, email, crypted_password, is_admin) VALUES ('admin', 'ad
 INSERT INTO users (name, email, crypted_password, self_introduction) VALUES ('chiikawa', 'chiikawa@example.com', '$2b$12$sBXqSJJ7fOJUAhUHAqq1TuAmgMmfsOiJzvNhwINozNwrtGl8gbqtG', 'フ！');
 INSERT INTO channels (description, name, start_date) VALUES ('superぼっち部屋へようこそ', 'superぼっち部屋', '2024-04-27');
 INSERT INTO messages (user_id, channel_id, message) VALUES (1, 1, 'ヤヤーンパパヒュパヒュパ');
-INSERT INTO badges (user_id, message_id, badge_type) VALUES (1, 1, 'GOLD');
+INSERT INTO badge_types (name) VALUES ('GOLD'), ('SILVER'), ('BRONZE');
+INSERT INTO badges (user_id, message_id, badge_type_id, created_by) VALUES (1, 1, 2, NULL);
 INSERT INTO personal_channels (user_id, name, description) VALUES (1, 'virtual_chiikawa', '草むしり検定5級目指して頑張ります！');
