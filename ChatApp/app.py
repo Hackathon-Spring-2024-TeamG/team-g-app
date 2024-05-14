2e382fee77e542e6a910af64379e84633db59e49
kentarousui <kentarousui1985920@gmail.com>
2024-05-14 22:48:27 +0900

    デプロイテスト用実装

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -216,4 +216,4 @@ def update_channel(channel_id):
         return redirect('/')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True, port=5000)
