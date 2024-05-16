4ea5f86a1cec70ad95e0976414903250f5da5fbe
kentarousui <kentarousui1985920@gmail.com>
2024-05-16 22:19:00 +0900

    テストデプロイ用コードに合わせた

--- ChatApp/util/DB.py
+++ ChatApp/util/DB.py
@@ -1,22 +1,49 @@
+# import pymysql
+
+# class DB:
+#     @staticmethod
+#     def getConnection():
+#         conn = None
+#         try:
+#             conn = pymysql.connect(
+#                 host="",
+#                 database="",
+#                 port=3306,
+#                 user="",
+#                 password="",
+#                 charset="utf8",
+#                 cursorclass=pymysql.cursors.DictCursor
+#             )
+#             return conn
+#         except pymysql.MySQLError as e:
+#             print("データベース接続エラー:", e)
+#             if conn:
+#                 conn.close()
+#             return None
+
+
+
+
+
+
+
 import pymysql
 
 class DB:
-    @staticmethod
     def getConnection():
-        conn = None
         try:
             conn = pymysql.connect(
-                host="",
-                database="",
-                port=3306,
-                user="",
-                password="",
-                charset="utf8",
-                cursorclass=pymysql.cursors.DictCursor
+#            host="db",
+            host="hackathonspring2024teamg-web.cdkwg66k0zmv.ap-northeast-1.rds.amazonaws.com",
+            database="chatapp",
+            port=3306,
+            user="testuser",
+            password="testuser",
+            charset="utf8",
+            cursorclass=pymysql.cursors.DictCursor
             )
             return conn
-        except pymysql.MySQLError as e:
-            print("データベース接続エラー:", e)
-            if conn:
-                conn.close()
-            return None
+        except (ConnectionError):
+            print("コネクションエラーです")
+            conn.close()
+
